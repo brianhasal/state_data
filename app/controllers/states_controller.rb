8f38a0e7b96795968bc985c78df5fcd077b1f6ce
Brian Hasal <brian.hasal@gmail.com>
2022-08-29 06:27:19 -0700

    Moved csv_parser to states_controller

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -3,10 +3,32 @@ require 'csv'
 
 class StatesController < ApplicationController
 
-  def index
-    states = CSV.open("abbreviations.csv", "w") do |csv|
-      csv << [:Code]
+  def csv_parser(filename)
+    
+    table = CSV.parse(File.read(filename), headers: true)
+
+    csv_array = []
+    out_array = []
+
+    table.each do |row|
+      csv_array << row  
     end
+
+    csv_array.each do |row|
+      temp_array = []
+      row.each do |key, value|
+        temp_array << {key => value}
+      end
+      out_array << temp_array
+    end
+    
+    
+    out_array.map { |array| array.inject(:merge) }
+
+  end
+
+  def index
+    states = csv_parser("abbreviations.csv")
     render json: states.as_json
   end
 
--- app/models/state.rb
+++ app/models/state.rb
@@ -1,29 +1,4 @@
-require 'csv'
-
-
 class State < ApplicationRecord
 
-  def csv_parser(filename)
-    
-    table = CSV.parse(File.read(filename), headers: true)
 
-    csv_array = []
-    out_array = []
-
-    table.each do |row|
-      csv_array << row  
-    end
-
-    csv_array.each do |row|
-      temp_array = []
-      row.each do |key, value|
-        temp_array << {key => value}
-      end
-      out_array << temp_array
-    end
-    
-    
-    out_array.map { |array| array.inject(:merge) }
-
-  end
 end
